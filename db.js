c4dff0c8fd1dcf624dd490f0fff18d6cc3747fcd
Eliezer <eliezer.somuah.es@gmail.com>
2025-04-26 13:05:43 +0000

    db

--- db.js
+++ db.js
@@ -1,13 +1,10 @@
-// backend/db.js
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
-  host:     process.env.PGHOST,
-  user:     process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  port:     process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // important for Render/Postgres cloud setups
+  },
 });
 
 module.exports = pool;
